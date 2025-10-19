55e303b02bb5cde6961009bac23abac8c396064c
Marcel Dragon <dragon.marcel@o2.pl>
2025-10-20 00:07:27 +0200

    chore: initial backend

---
+++ backend/src/main/java/pl/matgwiazda/MatGwiazdaApplication.java
@@ -0,0 +1,13 @@
+package pl.matgwiazda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatGwiazdaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatGwiazdaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pl/matgwiazda/MatGwiazdaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matgwiazda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatGwiazdaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
